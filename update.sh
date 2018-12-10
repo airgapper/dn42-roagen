199881dc795a73e213ac33efc7c03278f5f61b55
Christoffer <netravnen@gmail.com>
2018-12-10 17:33:34 +0100

    Readded update.sh
    o Now includes commit to dn42-rpki-export.json

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ISO_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+# Commit latest version of dn42-rpki-export.json
+git add dn42-rpki-export.json
+git commit dn42-rpki-export.json -m "Updated dn42-rpki-export.json - $ISO_DATE"
+
+# Push repository to every remote configured
+for REMOTE in $(git remote | paste -sd " " -) ; do git ps $REMOTE master ; done
