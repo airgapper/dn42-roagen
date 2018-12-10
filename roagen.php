3bec9ae84cdddb81184dcc05bdf9121534c903a2
Christoffer <netravnen@gmail.com>
2018-12-10 19:06:36 +0100

    Updated roagen.php
    Do not modify $maxLength to greater value than $mask if imported from ICVPN or CHAOSVPN

--- roagen.php
+++ roagen.php
@@ -77,7 +77,7 @@ foreach($files as $file)
                         $roas['roas'][$i]['asn'] = $asn[0];
                         $roas['roas'][$i]['prefix'] = $route;
                         if (isset($ta)) {
-                            if ($ta != 'ICVPN') {
+                            if ($ta != 'ICVPN' || $ta != 'CHAOSVPN') {
                                 $mask = ($mask <= 64 ? '64' : $mask);
                             }
                         } else {
@@ -92,7 +92,7 @@ foreach($files as $file)
                 $roas['roas'][$i]['asn'] = $asn[0];
                 $roas['roas'][$i]['prefix'] = $route;
                 if (isset($ta)) {
-                    if ($ta != 'ICVPN') {
+                    if ($ta != 'ICVPN' || $ta != 'CHAOSVPN') {
                         $mask = ($mask <= 64 ? '64' : $mask);
                     }
                 } else {
