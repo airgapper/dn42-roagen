880ef30c252def6bca863128d95696dfcf55717c
Christoffer <christoffer@netravnen.de>
2018-12-14 18:51:54 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 ISO_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-# Commit latest version of dn42-rpki-export.json
-git add dn42-rpki-export.json
-git commit dn42-rpki-export.json -m "Updated dn42-rpki-export.json - $ISO_DATE"
+# Commit latest version of ROA files
+git add roa/*
+git commit roa/* -m "Updated ROA files - $ISO_DATE"
 
 # Commit latest version of dn42-rfc8416-export.json
 git add dn42-rfc8416-export.json
