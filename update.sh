fe996e6727b4a7e7db64ba8d1e59923f763b2efc
Christoffer <christoffer@netravnen.de>
2018-12-14 15:27:50 +0000

    update.sh now also commits dn42-rfc8416-export.json if previously updated

--- update.sh
+++ update.sh
@@ -6,5 +6,9 @@ ISO_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 git add dn42-rpki-export.json
 git commit dn42-rpki-export.json -m "Updated dn42-rpki-export.json - $ISO_DATE"
 
+# Commit latest version of dn42-rfc8416-export.json
+git add dn42-rfc8416-export.json
+git commit dn42-rfc8416-export.json -m "Updated dn42-rfc8416-export.json - $ISO_DATE"
+
 # Push repository to every remote configured
 for REMOTE in $(git remote | paste -sd " " -) ; do git ps $REMOTE master ; done
