875538daed60d22b64634f3a4f1516f0bcad60f5
Chriztoffer <chriztoffer@netravnen.de>
2020-02-02 20:08:41 +0000

    Update 'lib/functions.php'
    Remove git commit info from bird roa/route files

--- lib/functions.php
+++ lib/functions.php
@@ -65,13 +65,17 @@ function writeBirdConfig ($roas)
   $bird2_fq4 = fopen ('roa/bird4_route_dn42.conf', 'w');
   $bird2_fq6 = fopen ('roa/bird6_route_dn42.conf', 'w');
 
+  /*
   fwrite ($bird1_fq,  shell_exec ("/usr/bin/git -C ../registry/ log -n 1  --merges | sed 's/^/# /g'"));
   fwrite ($bird1_fq4, shell_exec ("/usr/bin/git -C ../registry/ log -n 1  --merges | sed 's/^/# /g'"));
   fwrite ($bird1_fq6, shell_exec ("/usr/bin/git -C ../registry/ log -n 1  --merges | sed 's/^/# /g'"));
+  */
 
+  /*
   fwrite ($bird2_fq,  shell_exec ("/usr/bin/git -C ../registry/ log -n 1  --merges | sed 's/^/# /g'"));
   fwrite ($bird2_fq4, shell_exec ("/usr/bin/git -C ../registry/ log -n 1  --merges | sed 's/^/# /g'"));
   fwrite ($bird2_fq6, shell_exec ("/usr/bin/git -C ../registry/ log -n 1  --merges | sed 's/^/# /g'"));
+  */
 
   foreach ($roas["roas"] as $roa)
   {
